5b7ad185a8b945ba55ef7011cad4d19ec1dab991
Bishwas Mishra <b.bishwas@gmail.com>
2023-05-31 11:03:50 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,59 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.exc import OperationalError
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-
-import mysql.connector
-
-# Establish a connection to the MySQL server
-connection = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    #password='password'
-)
-
-# Create a cursor object to execute SQL commands
-cursor = connection.cursor()
-
-# Execute the raw SQL command to create a database
-database_name = 'deals'
-create_database_command = f'CREATE DATABASE {database_name}'
-try:
-    cursor.execute(create_database_command)
-except:
-    print('Database ALready Exists')
-# Close the cursor and the connection
-cursor.close()
-connection.close()
-
-# Establish a connection to the MySQL server again
-connection = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    #password='password',
-    database='deals'  # Specify the database you created
-)
-
-# Create a cursor object to execute SQL commands
-cursor = connection.cursor()
-
-# Execute the raw SQL command to create the "deal" table
-create_table_command = """
-CREATE TABLE deal (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    price VARCHAR(20) NOT NULL,
-    discount VARCHAR(20) NOT NULL
-)
-"""
-try:
-    cursor.execute(create_table_command)
-    connection.commit()
-except:
-    print('error while creating table, table exists or other problem [Line 53 Config.py]')
-
-cursor.close()
-connection.close()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/deals?unix_socket=C:/xampp/mysql/mysql.sock'
@@ -63,9 +14,34 @@ migrate = Migrate(app, db)
 # Import your models here, if needed
 # from models import ...
 
-# Register blueprints and configure routes here, if needed
-# ...
+
+def create_database():
+    try:
+        # Create a database engine and session
+        engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Create the database using a transaction
+        session.execute("CREATE DATABASE IF NOT EXISTS deals")
+        session.commit()
+        session.close()
+
+    except OperationalError:
+        print('Error occurred while creating the database.')
+
+
+def create_table():
+    try:
+        # Create all tables using migrations
+        with app.app_context():
+            db.create_all()
+
+    except OperationalError:
+        print('Error occurred while creating the table.')
+
 
 if __name__ == '__main__':
+    create_database()
+    create_table()
     app.run()
-
