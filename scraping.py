91319478c1b4677052388f62be86bb7136267041
Bishwas Mishra <b.bishwas@gmail.com>
2023-05-31 12:12:30 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,28 +1,32 @@
-
 import requests
 from bs4 import BeautifulSoup
 from models.deal import Deal
 from config import db
 
 def scrape_deals():
-    url = "https://www.amazon.com"  # Replace with the URL of the specific Amazon page you want to scrape
+    url = "https://www.amazon.com/gp/goldbox"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
     deals = []
 
     # Locate and extract the deal information using BeautifulSoup
-    # For example:
-    deal_elements = [1,2,3]
+    deal_elements = soup.select(".dealTile")
     for deal_element in deal_elements:
+        title_element = deal_element.select_one(".dealTitle")
+        price_element = deal_element.select_one(".priceBlock")
+        discount_element = deal_element.select_one(".dealPriceText")
 
+        if title_element and price_element and discount_element:
+            title = title_element.text.strip()
+            price = price_element.text.strip()
+            discount = discount_element.text.strip()
 
-        deal = Deal(title=1, price=1, discount=1)
-        deals.append(deal)
+            deal = Deal(title=title, price=price, discount=discount)
+            deals.append(deal)
 
     # Store the scraped deals in the database
     with db.session.begin_nested():
         for deal in deals:
             db.session.add(deal)
     db.session.commit()
-
